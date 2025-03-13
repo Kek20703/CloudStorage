99e879941d3023306809531f9f1a90733cc62057
legotin212 <99343368+legotin212@users.noreply.github.com>
2025-03-13 22:50:06 +0500

    refactor: make  return SignInResponseDto instead of ResponseEntity

--- src/main/java/org/example/cloudstorage/controller/UserController.java
+++ src/main/java/org/example/cloudstorage/controller/UserController.java
@@ -1,21 +1,18 @@
 package org.example.cloudstorage.controller;
 
-import org.example.cloudstorage.dto.response.SignUpResponseDto;
-import org.springframework.http.ResponseEntity;
+import org.example.cloudstorage.dto.response.SignInResponseDto;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user/me")
-
 public class UserController {
     @GetMapping()
-    public ResponseEntity<SignUpResponseDto> home(@AuthenticationPrincipal UserDetails userDetails) {
+    public SignInResponseDto getUsername(@AuthenticationPrincipal UserDetails userDetails) {
 
-        return ResponseEntity.ok(new SignUpResponseDto(userDetails.getUsername()));
+        return new SignInResponseDto(userDetails.getUsername());
     }
 }
