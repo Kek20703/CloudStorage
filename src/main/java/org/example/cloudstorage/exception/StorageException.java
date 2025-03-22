752245edf7f68ecd6f576fd4fe825e6fefbec8d2
legotin212 <99343368+legotin212@users.noreply.github.com>
2025-03-23 00:24:28 +0500

    feat: add StorageException

---
+++ src/main/java/org/example/cloudstorage/exception/StorageException.java
@@ -0,0 +1,7 @@
+package org.example.cloudstorage.exception;
+
+public class StorageException extends RuntimeException {
+    public StorageException(String message) {
+        super(message);
+    }
+}
