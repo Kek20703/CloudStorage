460f048bd2b6e3cf1f90453d76b1f467ece9e522
legotin212 <99343368+legotin212@users.noreply.github.com>
2025-03-23 00:24:43 +0500

    feat: add StorageService interface

---
+++ src/main/java/org/example/cloudstorage/service/StorageService.java
@@ -0,0 +1,29 @@
+package org.example.cloudstorage.service;
+
+import org.example.cloudstorage.dto.response.ResourceInfoResponseDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface StorageService {
+
+    void createDefaultUserDirectory(Long userId);
+
+    void save(Long userId, String path, MultipartFile file);
+
+    void delete(Long userId, String path);
+
+    InputStream get(Long userId, String path);
+
+    ResourceInfoResponseDto rename(Long userId, String oldPath, String newPath);
+
+    ResourceInfoResponseDto getInfo(Long userId, String path);
+
+    ResourceInfoResponseDto createEmptyDirectory(Long userId, String path);
+
+    List<ResourceInfoResponseDto> search(Long userId, String path);
+
+    List<ResourceInfoResponseDto> getDirectoryContentInfo(Long userId, String path);
+
+}
