d742efaf86aefdc91180b1dad87de6460b19a343
legotin212 <99343368+legotin212@users.noreply.github.com>
2025-04-08 11:57:03 +0500

    Style: remove unused @SneakyThrows

--- src/main/java/org/example/cloudstorage/controller/UserController.java
+++ src/main/java/org/example/cloudstorage/controller/UserController.java
@@ -1,8 +1,6 @@
 package org.example.cloudstorage.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 import org.example.cloudstorage.dto.response.auth.SignInResponseDto;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,14 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user/me")
-@Slf4j
 @RequiredArgsConstructor
 public class UserController {
 
-    @SneakyThrows
     @GetMapping()
     public SignInResponseDto getUsername(@AuthenticationPrincipal UserDetails userDetails) {
-
         return new SignInResponseDto(userDetails.getUsername());
     }
 }
